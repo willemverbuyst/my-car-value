41cb04f3e054bdad52e8ef066ec8de1d7f7b9186
Willem Verbuyst <willemverbuyst@gmail.com>
2023-07-31 07:34:18 +0200

    Test signin error and password

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,4 +1,4 @@
-import { BadRequestException } from '@nestjs/common';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 
 import { AuthService } from './auth.service';
@@ -45,5 +45,35 @@ describe('AuthService', () => {
     await expect(service.signup('asdf@lklj.com', 'asdf')).rejects.toThrow(
       BadRequestException,
     );
+  });
+
+  it('throws an error if signin is called with an unused email', async () => {
+    await expect(service.signin('qwer@tyui.io', '123')).rejects.toThrow(
+      NotFoundException,
+    );
+  });
+
+  it('throws an error if an invalid password is provided', async () => {
+    fakeUsersService.find = () =>
+      Promise.resolve([{ email: 'asdf@asdf.com', password: 'asdf' } as User]);
+
+    await expect(service.signin('qerw@waer.com', 'sdf')).rejects.toThrow(
+      BadRequestException,
+    );
+  });
+
+  it('returns a user if correct password is provided', async () => {
+    fakeUsersService.find = () =>
+      Promise.resolve([
+        {
+          email: 'asdf@asdf.com',
+          password:
+            '66e80fcd577ed9b6.76de9fd37815bb2406ea8bf9836e556461d83f43aa75078ae9f76a1617673d5b',
+        } as User,
+      ]);
+
+    const user = await service.signin('asdf@saf.com', 'password');
+
+    expect(user).toBeDefined();
   });
 });
