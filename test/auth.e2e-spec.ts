f7e6e4877c610973bf90dc8e0c9fe03f6821995f
Willem Verbuyst <willemverbuyst@gmail.com>
2023-08-02 05:29:19 +0200

    Test currently logged in user

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -15,9 +15,9 @@ describe('AuthController (e2e)', () => {
     await app.init();
   });
 
-  it('handles a signup request', () => {
-    const email = 'addfasf@asdf.com';
-    return request(app.getHttpServer())
+  it('handles a signup request', async () => {
+    const email = 'foo@bar.com';
+    return await request(app.getHttpServer())
       .post('/auth/signup')
       .send({ email, password: 'password' })
       .expect(201)
@@ -26,5 +26,21 @@ describe('AuthController (e2e)', () => {
         expect(id).toBeDefined();
         expect(email).toEqual(email);
       });
+  });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = 'test@test.io';
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'password' })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie);
+
+    expect(body.email).toEqual(email);
   });
 });
