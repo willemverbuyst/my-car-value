e06b72fa9d91b0ec5a57945e0cd420f20db8f9f5
Willem Verbuyst <willemverbuyst@gmail.com>
2023-08-03 04:20:33 +0200

    Set up controller

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from 'src/guards/auth.guard';
+import { CreateReportDto } from './dto/create-report.dto';
+import { ReportsService } from './reports.service';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+  constructor(private reportsService: ReportsService) {}
+
+  @UseGuards(AuthGuard)
+  @Post('/')
+  createReport(@Body() body: CreateReportDto) {
+    return this.reportsService.create(body);
+  }
+}
