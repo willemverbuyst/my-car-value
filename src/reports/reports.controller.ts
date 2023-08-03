f8499a23978e8fa3a2e1511ab13881d178f3a12b
Willem Verbuyst <willemverbuyst@gmail.com>
2023-08-03 05:08:04 +0200

    Use dto to include userId

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,16 +1,19 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { AuthGuard } from '../guards/auth.guard';
 import { CurrentUser } from '../users/decorators/current-user.decorator';
 import { User } from '../users/user.entity';
 import { CreateReportDto } from './dto/create-report.dto';
+import { ReportDto } from './dto/report.dto';
 import { ReportsService } from './reports.service';
 
 @Controller('reports')
 export class ReportsController {
   constructor(private reportsService: ReportsService) {}
 
-  @UseGuards(AuthGuard)
   @Post('/')
+  @UseGuards(AuthGuard)
+  @Serialize(ReportDto)
   createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
     return this.reportsService.create(body, user);
   }
