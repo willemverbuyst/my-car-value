8caaef1058ba3041a0f5b24096c796d6003093c4
Willem Verbuyst <willemverbuyst@gmail.com>
2023-08-04 05:32:20 +0200

    Catch query string

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,9 +1,11 @@
 import {
   Body,
   Controller,
+  Get,
   Param,
   Patch,
   Post,
+  Query,
   UseGuards,
 } from '@nestjs/common';
 import { AdminGuard } from 'src/guards/admin.guard';
@@ -13,6 +15,7 @@ import { CurrentUser } from '../users/decorators/current-user.decorator';
 import { User } from '../users/user.entity';
 import { ApproveReportDto } from './dto/approve-report.dto';
 import { CreateReportDto } from './dto/create-report.dto';
+import { GetEstimateDto } from './dto/get-estimate.dto';
 import { ReportDto } from './dto/report.dto';
 import { ReportsService } from './reports.service';
 
@@ -20,7 +23,7 @@ import { ReportsService } from './reports.service';
 export class ReportsController {
   constructor(private reportsService: ReportsService) {}
 
-  @Post('/')
+  @Post()
   @UseGuards(AuthGuard)
   @Serialize(ReportDto)
   createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
@@ -31,5 +34,10 @@ export class ReportsController {
   @UseGuards(AdminGuard)
   approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
     return this.reportsService.changeApproval(id, body.approved);
+  }
+
+  @Get()
+  async getEstimate(@Query() query: GetEstimateDto) {
+    console.log(query);
   }
 }
